89ef232523d993ee847e617c0b0be8656a93f84e
Lime5005 <zlp5005@gmail.com>
2021-06-15 17:49:12 +0200

    Add a Main class to prompt the program, add javadoc

---
+++ Project02Eclipse/src/Main.java
@@ -0,0 +1,8 @@
+import com.hemebiotech.analytics.AnalyticsCounter;
+
+public class Main {
+    public static void main(String[] args) {
+        AnalyticsCounter analyticsCounter = new AnalyticsCounter();
+        analyticsCounter.startAnalysis();
+    }
+}
--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,8 +1,8 @@
 package com.hemebiotech.analytics;
 
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -10,43 +10,66 @@ import java.util.TreeSet;
 
 public class AnalyticsCounter {
 
-	public static void main(String[] args) {
-		List<String> result;
+	/**
+	 * This method will prompt all the other methods.
+	 */
+	public void startAnalysis() {
+		// Reading:
+		List<String> symptomList;
 		String path = "/Users/lime/IdeaProjects/Project2_DA_Java_biotech/Project02Eclipse/symptoms.txt";
-		result = new ReadSymptomDataFromFile(path).GetSymptoms();
+		symptomList = readingFile(path);
+		// Sorting:
+		ArrayList<String> sortedData = sortingFile((ArrayList<String>) symptomList);
+		// Saving:
+		savingFile(sortedData);
 
-		ArrayList<String> sortedData = countFrequencies((ArrayList<String>) result);
-		writeFile(sortedData);
 	}
 
 	/**
-	 * Using Collections API to get the occurrence of each string in an array list.
-	 * @param list the list to analyse
-	 * @return  a string list
+	 * This method is to read and put all symptoms in a list.
+	 * @param path Where located the text file to analysis.
+	 * @return The raw data(symptoms) in a list or an empty list if no data.
 	 */
-	public static ArrayList<String> countFrequencies(ArrayList<String> list) {
-
-		// Add all your objects to the TreeSet, you will get a sorted Set.
-		TreeSet<String> ts = new TreeSet<String>(list);
-		ArrayList<String> newResult = new ArrayList<>();
-		for (String s : ts) {
-//			System.out.println(s + "=" + Collections.frequency(list, s));
-			newResult.add(s + "=" + Collections.frequency(list, s));
-		}
-		return newResult;
+	public List<String> readingFile(String path) {
+		List<String> result;
+		result = new ReadSymptomDataFromFile(path).GetSymptoms();
+		return result;
 	}
 
-	public static void writeFile(ArrayList<String> list) {
+
+	/**
+	 * Using TreeSet to get all symptoms sorted alphabetically,
+	 * and make sure no duplicate elements,
+	 * then count the occurrence of each symptom in the list.
+	 * @param symptoms The list to analysis.
+	 * @return  A sorted and counted list naming "sortedSymptoms".
+	 */
+	private ArrayList<String> sortingFile(ArrayList<String> symptoms) {
+
+		TreeSet<String> treeSetSymptoms = new TreeSet<>(symptoms);
+		ArrayList<String> sortedSymptoms = new ArrayList<>();
+		for (String symptom : treeSetSymptoms) {
+			sortedSymptoms.add(symptom + "=" + Collections.frequency(symptoms, symptom));
+		}
+
+		return sortedSymptoms;
+	}
+
+	/**
+	 * This method will write the sortedSymptoms line by line in a new file "results.out".
+	 * @param list The sortedSymptoms from the method above "sortingFile".
+	 */
+	public void savingFile(ArrayList<String> list) {
 		PrintWriter writer;
 		try {
-			writer = new PrintWriter("results.out", "UTF-8");
+			writer = new PrintWriter("results.out", StandardCharsets.UTF_8);
 
 			for (String st : list) {
 				writer.println(st);
 			}
 			writer.close();
 
-		} catch (FileNotFoundException | UnsupportedEncodingException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
